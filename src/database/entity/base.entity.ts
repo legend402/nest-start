b881da066fb979edc90f831cda78de216004fb08
hanyj <1689699478@qq.com>
2023-02-14 16:58:43 +0800

    chore: 修改时间类型

--- src/database/entity/base.entity.ts
+++ src/database/entity/base.entity.ts
@@ -2,12 +2,18 @@ import { Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class BaseEntity {
   @CreateDateColumn({
-    name: 'create_time'
+    name: 'create_time',
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP",
+    precision: 6,
   })
   createTime!: Date;
 
   @UpdateDateColumn({
-    name: 'update_time'
+    name: 'update_time',
+    type: "timestamp",
+    default: () => "CURRENT_TIMESTAMP",
+    precision: 6,
   })
   updateTime!: Date;
 
