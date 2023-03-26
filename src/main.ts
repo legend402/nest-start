480e9d7138e6b4920a323e729440791bef37f858
hanyj <1689699478@qq.com>
2023-03-26 11:20:43 +0800

    feat: 添加https服务

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,25 @@ import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './logical/validate/validationPipe';
 import { JwtAuthGuard } from './logical/auth/JwtAuthGuard';
+import { readFileSync } from 'fs';
+import { join } from 'path';
+import * as express from 'express';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as http from 'http'
+import * as https from 'https'
+const httpsOptions = {
+  key: readFileSync(join(process.cwd(), 'ssl.key')),
+  cert: readFileSync(join(process.cwd(), 'ssl.pem')),
 
+}
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const server = express()
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  app.enableCors()
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards(new JwtAuthGuard(new Reflector()));
-  await app.listen(3010);
+  await app.init();
+  http.createServer(server).listen(3010);
+  https.createServer(httpsOptions, server).listen(443);
 }
 bootstrap();
